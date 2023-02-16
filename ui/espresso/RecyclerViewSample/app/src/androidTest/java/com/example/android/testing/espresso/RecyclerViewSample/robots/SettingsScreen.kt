8fdebfcba75f190e117285d9ddae7b8268bbdbaa
Scott D'Antuono <scott.dantuono@onepeloton.com>
2023-02-16 16:53:20 -0500

    trying to create settings object variables

--- ui/espresso/RecyclerViewSample/app/src/androidTest/java/com/example/android/testing/espresso/RecyclerViewSample/robots/SettingsScreen.kt
+++ ui/espresso/RecyclerViewSample/app/src/androidTest/java/com/example/android/testing/espresso/RecyclerViewSample/robots/SettingsScreen.kt
@@ -1,11 +1,15 @@
 package com.example.android.testing.espresso.RecyclerViewSample.robots
 
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.ViewAssertion
 import androidx.test.espresso.ViewInteraction
+import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
-import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.espresso.contrib.RecyclerViewActions
+import androidx.test.espresso.matcher.ViewMatchers.*
 import com.example.android.testing.espresso.RecyclerViewSample.R
+import com.example.android.testing.espresso.RecyclerViewSample.tests.RecyclerViewSampleTest
+import org.hamcrest.CoreMatchers.allOf
 
 class SettingsScreen {
 
@@ -16,19 +20,36 @@ class SettingsScreen {
     /*
     Hint for toggles - because each toggle is the same, we need to match against neighboring views
 
-    Settings
+    Settingsoi8
         Row 1
             Label - Toggle
         Row 2
             Label - Toggle
     So we want to find the row 1 toggle whose sibling in the hierarchy is Row 1 Label
      */
-
     private val negativeItemLabel: ViewInteraction = onView(withText(R.string.settings_toggle_negative_items))
-    private val negativeItemToggle: ViewInteraction = TODO("Not yet implemented")
+    private val negativeItemToggle: ViewInteraction = onView(allOf(
+        withId(R.id.switchWidget),
+        hasSibling(withText(R.string.settings_toggle_negative_items))
+    ))
 
-    private val middleRowLabel: ViewInteraction = TODO("Not yet implemented")
-    private val middleRowToggle: ViewInteraction = TODO("Not yet implemented")
+    private val middleRowLabel: ViewInteraction = onView(withText(R.string.middle))
+    private val middleRowToggle: ViewInteraction = onView(
+        allOf(
+            withId(R.id.switchWidget),
+            hasSibling(withText(R.string.middle))
+        )
+    )
+
+    fun tapOnNegativeItemToggle() {
+        negativeItemLabel.check(matches(isDisplayed()))
+        negativeItemToggle.perform(click())
+    }
+
+    fun tapMiddleRowToggle() {
+        middleRowLabel.check(matches(isDisplayed()))
+        middleRowToggle.perform(click())
+    }
 
     init {
         // we can also use constructors / init blocks to do things each time SettingsScreen() is invoked
